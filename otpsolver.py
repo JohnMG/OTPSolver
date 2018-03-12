45d1452da0a05ac0fc89ac1402f3796007f89c1c
JohnMG <jmassygreene@hotmail.com>
2018-03-13 10:34:06 +1100

    Comments added

--- otpsolver.py
+++ otpsolver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 #Author: John Massy-Greene
-#Program Details: An OTP Calculator
+#Program Details: An OTP Calculator that is created partially in accordance with
+#                 RFC4226 and RFC6238
 #Date: 11/09/2017
 #Update: 6/3/2018
 #Version 2 of TOTPCalc.py
@@ -18,6 +19,7 @@ import sys
 import re
 
 
+#global variables that help in the calculation of the one time pin
 digits = 6
 timeStep = 30
 #initialTime = T0
@@ -26,19 +28,25 @@ sharedKey = ""
 counter = 0
 verbose = False
 totp = False
+#extra args are all the arguments that are not mandatory
 extraArgs = ["d=","--timebased", "ts=", "--verbose", "T0=", "hash=", "key="]
-#maxCount should not be 4294967296. This is maximum value of 4 bytes signed
-#changed maxCount to (2**63)-1 which is the maximum value of 8 bytes signed
-#Later on in the program the sign gets masked out anway
+#maxCount has a value of (2**63)-1 which is the maximum value of 8 bytes signed
+#Later on in the program the sign gets masked out.
 maxCount = ((2**63)-1)
 hashAlgorithm = hashlib.sha1
-#keyEncoding will be a string that tells the program to decode the key
+#keyEncoding is a string that tells the program to decode the key
 #based on whether it is hexademical, base32 or base64. Default is base32
 keyEncoding = "base32"
 verboseInfo = ["---------- Algorithm Variables -----------------------------\n"]
 
 
 
+'''
+Hanndle Args function handles two sets of arguments.
+The first set of arguments is the mandatory positional arguments which are the
+key and the counter value. The second set of arguments are optional arguments
+which just affect how the algorithm interprets the key and the counter
+'''
 #The return codes are as follows:
 # 0 = Arguments handled successfully
 # 1 = Arguments given are not correct
@@ -46,9 +54,12 @@ def handleArgs():
     global verbose
     global totp
 
+#the program must have 2 arguments (key and counter)
+#or a maximum of nine.
     minArgs = 2
     maxArgs = 9
-    
+
+#remove the name of the program as an argument.   
     argsLen = len(sys.argv)
     args = sys.argv[1:argsLen]
     argsLen -= 1
@@ -56,13 +67,17 @@ def handleArgs():
     
     if(argsLen < minArgs or argsLen > maxArgs):
         return 1
-    
+
+#start the handling of optional arguments    
     if(argsLen >= 3):
         #check to make sure no weird args passed
         checkArgs = args[2:]
         if(check_bad_args(checkArgs) == True):
             return 1
-        
+
+#the code for digits checking is the similiar for every other argument
+#check to see if user passed at least one digit argument
+#if the user tried to use the argument twice then its invalid 
         digits = [x for x in args if extraArgs[0] in x]
         if(len(digits) >= 1):
             if(len(digits) > 1):
@@ -77,13 +92,16 @@ def handleArgs():
             else:
                 return 1
             
+        #check for initial time after checking for timebool but before the counter
+        #want to make sure that its only being used for timebased pins.
         initTimeCheck = [x for x in args if extraArgs[4] in x]
         if(len(initTimeCheck) >= 1):
             if(len(initTimeCheck) > 1):
                 return 1
             if(check_initial_time(initTimeCheck[0]) == 0):
                 return 1
-        
+
+        #check the time time step
         tsCheck = [x for x in args if extraArgs[2] in x]
         if(len(tsCheck) >= 1):
             if(len(tsCheck) == 1):
@@ -92,6 +110,7 @@ def handleArgs():
             else:
                 return 1
 
+        #check if the user wants verbose output
         verbCheck = [x for x in args if extraArgs[3] in x]
         if(len(verbCheck) >= 1):
             if(len(verbCheck) == 1):
@@ -99,6 +118,7 @@ def handleArgs():
             else:
                 return 1
 
+        #check for the type of hashing algorithm user wants
         hashCheck = [x for x in args if extraArgs[5] in x]
         if(len(hashCheck) >= 1):
             if(len(hashCheck) == 1):
@@ -107,6 +127,7 @@ def handleArgs():
             else:
                 return 1
 
+        #check for the particular key encoding.
         keyCheck = [x for x in args if extraArgs[6] in x]
         if(len(keyCheck) >= 1):
             if(len(keyCheck) == 1):
@@ -114,8 +135,8 @@ def handleArgs():
                     return 1
             else:
                 return 1
-#check for initial time after checking for timebool but before the counter
-#add code to handle the case that initial time comes before the counter
+
+    #Finally check the mandatory arguments of the secret key and the counter
     if(check_shared_key(args[0]) == 0):
         return 1
 
@@ -124,8 +145,12 @@ def handleArgs():
 
     return 0
         
-#NEED EXTRA CHECKING TO MAKE SURE WEIRD INPUT ISN'T GIVEN TO COMMANDS
-#This check function is sorta awful
+'''Verify that all arguments given conform to a legitimate format
+that can be processed by the program. This function will attempt
+to pattern match an argument against all possible types of arguments
+if a match isn't found then its a bad argument and the function returns
+true
+'''
 def check_bad_args(argList):
     result = False
     argPatterns = ["d=[0-9]+",
@@ -144,6 +169,11 @@ def check_bad_args(argList):
         
     return result
 
+'''
+Determine what type of encoding the user wants the program
+to recognise their key as. A key can either be in
+hexademical, base 32 or base64
+'''
 def check_key_type(keyType):
     global keyEncoding
     result = 0
@@ -157,8 +187,18 @@ def check_key_type(keyType):
 
     return result
 
-#The shared key must be at least 128 bits and recommended 160 according to RFC4226
-#Will not enforce this. But will instead provide warning.
+'''
+This function attempts to decode the key according to the given key encoding specified
+into a byte type format. If it can't do that then it returns an error. 
+
+Note:
+According to RFC4226 the shared key must be at least 128 bits and is recommended to be 160bits
+in the case of sha1. For sha256 and sha512 the key should be the respective sizes of the hmac.
+However when looking at googles authenticator it appears they will accept keys of minimum length
+of 80 bits(10 bytes). In the interests of interoperability this program will not enforce a
+minumum bit lengthand the user must use their own discretion when attempting to form a
+secure one time pin
+'''
 def check_shared_key(key):
     global sharedKey
     global keyEncoding
@@ -190,7 +230,11 @@ def check_shared_key(key):
             result = 0
 
     return result
-            
+
+'''
+Check to make sure the digits specified are integers and are in the
+range of 6 to 8 which is what the RFCs seem to specify
+'''
 def check_digit_args(digit):
     global digits
     digArgP = "^d=([0-9]+)$"
@@ -208,22 +252,38 @@ def check_digit_args(digit):
         print("digits must be 6 or 8")
     return result
 
+'''
+Make sure the timestep is a positive number and is only used
+in the time based algorithm
+'''
 def check_time_step(timer):
     global timeStep
+    global totp
 
     timeP = "^ts=([0-9]+)$"
     result = 0
 
-    tmatcher = re.compile(timeP)
-    matching = tmatcher.match(timer)
-    if(matching):
-        timeStep = int(matching.group(1))
-        if(timeStep > 0):
-            result = 1
+    if(totp):
+        tmatcher = re.compile(timeP)
+        matching = tmatcher.match(timer)
+        if(matching):
+            timeStep = int(matching.group(1))
+            if(timeStep > 0):
+                result = 1
+
     if(result <= 0):
-        print("Timestep must be a positive number")
+        if(totp):
+            print("Timestep must be a positive number")
+        else:
+            print("Timestep is only suitable for time based OTP")
     return result
 
+'''
+Check the type of hashing algorithm the user wants to use
+The HOTP RFC only specifies sha1. While TOTP RFC allows
+sha256 and sha512 to be used as well. This program doesn't
+restrict the user from using sha256 and sha512 in HOTP
+'''
 def check_hashing(hashType):
     global hashAlgorithm
     result = 0
@@ -248,6 +308,7 @@ def check_hashing(hashType):
 
     return result
 
+#check the counter given to program
 def check_counter(count):
     global counter
     global totp
@@ -256,6 +317,9 @@ def check_counter(count):
     result = 0
     intC = 0
 
+#if the user is using normal counter based algorithm
+#then make sure the counter is digits that are
+#more than zero and less than the maximum of 8 bytes signed.
     if(totp == False):
         if(count.isdigit() == False):
             print("Counter must be an integer value >= 0")
@@ -267,8 +331,12 @@ def check_counter(count):
                 print("Counter must be an integer value >= 0")
             else:
                 counter = int(count)
-                result = 1 
-
+                result = 1
+                
+#if the user is using timebased then they passed in a non digit
+#argument. Check this argument with general_time_check
+#Also need to make sure the the counter is more than the initial time
+#otherwise you will get a negative counter.
     else:
         timeResult = general_time_check(count)
         if(timeResult != -1):
@@ -280,6 +348,7 @@ def check_counter(count):
 
     return result
 
+#Use general_time_check to see if T0 is a valid time/
 def check_initial_time(aTime):
     global initialTime
     global totp
@@ -296,10 +365,9 @@ def check_initial_time(aTime):
     return result
 
 
-#If regular count then its a value between 0 and 4294967296
-#If time based value then its either string "now" or
+#If counter is a time based value then its either string "now" or
 #or a date time of the form "YYYY:MM:DD:hh:mm:ss:{milliseconds}"
-#where milliseconds is optional
+#where milliseconds is optional.
 def general_time_check(aTime):
     
     result = -1
@@ -309,6 +377,8 @@ def general_time_check(aTime):
     matcher = re.compile(timePattern1)
     matches = matcher.match(aTime)
     if(matches):
+        #pass the string to handle_custom_time to build an integer
+        #in UTC epoch format
         timeHandled = handle_custom_time(matches)
         if(timeHandled != -1):
             result = timeHandled
@@ -321,10 +391,9 @@ def general_time_check(aTime):
     return result
 
 
-#Found after some testing this function handles dates past the time
-#07/02/2016 06:28:16
-# result will be -1 when the time is not correct as all correct times
-# in Unix epoch are >= 0
+#handle custom time gets the result from a regex match
+#and extracts the year, month, day, hour, minute and second
+#from the matched groups.
 def handle_custom_time(regmatch):
     result = -1
     
@@ -334,11 +403,16 @@ def handle_custom_time(regmatch):
     hour = regmatch.group(4)
     minu = regmatch.group(5)
     sec = regmatch.group(6)
+    #python doesn't seem to handle milliseconds for epoch time
+    #probably because its meant to be an integer. So grab the
+    #milliseconds and add them at the end
     if(regmatch.group(8) != None):
         milli = float(regmatch.group(8))/1000
     else:
         milli = float(0)
 
+    #make a time struct from the information and then pass it to timegm
+    #to calculate the seconds from the UNIX epoch
     try:
         tup = time.strptime("{} {} {} {} {} {}"
                             .format(year, month, day, hour, minu, sec),
@@ -350,16 +424,18 @@ def handle_custom_time(regmatch):
 
     return result
     
-
+#function that prints what the program expects as a format for
+#a time variable
 def print_time_error():
     print("time must be either be the string \"now\" or of the forms:")
     print("- YYYY:MM:DD:hh:mm:ss")
     print("- YYYY:MM:DD:hh:mm:ss:xxx")
     print("- where hh is 24 hour time and xxx is milliseconds")
         
-        
+#print a small guide in a similiar manner to unix MAN pages
+#On how to use the program.       
 def proper_usage():
-    print("Usage: otpsolver.py [key] [[counter] or [time]] [d=digits]")
+    print("\nUsage: otpsolver.py [key] [[counter] or [time]] [d=digits]")
     print("                    [--timebased] [ts=timestep] [--verbose]")
     print("                    [T0=time] [hash=sha1|sha256|sha512]")
     print("                    [key=hex|base32|base64]\n")
@@ -380,9 +456,7 @@ def proper_usage():
     print("key         => what encoding the key is supposed to use. Default is base32")
 
 '''
-Note to self: this function is here so that in future
-you can test whether the key given is base64, base32 or hex
-For the moment it only converts base32 keys to bytes
+Decode the key from the specified encoding format into bytes
 '''
 def key_to_bytes(key):
     global keyEncoding
@@ -398,6 +472,9 @@ def key_to_bytes(key):
         
     return result
 
+#the TOP agorithm does a custom calculation of the time
+#makes it into a suitable counter value and then passes
+#the values into to hotp algorithm.
 def totp_algorithm(key, count):
     global initialTime
     global timeStep
@@ -411,36 +488,41 @@ def totp_algorithm(key, count):
     result = hotp_algorithm(key, T)
     return result
 
-
+#the base Hash-MAC One Time Pin Algorithm
 def hotp_algorithm(key, count):
     global digits
     global hashAlgorithm
     global verbose
     global verboseInfo
-    
+
+    #mask one is used to get the lower 4 bits of the last byte of the digest
     mask1 = 0x0f
+    #mask2 is used to make the digest/one time pin unsigned.
     mask2 = 0x7fffffff
     hashAlg = hashAlgorithm
     
     byteKey = key_to_bytes(key)
     byteCounter = count.to_bytes(8, byteorder='big', signed=False)
 
+    #make the digest from the key, counter and hashing algorithm
     hmacsha = hmac.new(byteKey, byteCounter, hashAlg)
     HS = hmacsha.digest()
     HSlen = len(HS)-1
-    
+    #the offset is gathered from the last 4 bits of the the digest
     offset = HS[HSlen]&mask1
+    #get the 4 byte code from the offset
     fullCode = HS[offset:offset+4]
     fullCodeNum = int.from_bytes(fullCode, byteorder='big', signed=False)
     fullCodeNum = fullCodeNum&mask2
 
+    #get the final one time pin digits.
     finalCode = fullCodeNum % (10**digits)
     finalCodeString = str(finalCode)
     while(len(finalCodeString) < digits):
         finalCodeString = "0" + finalCodeString
     finalCode = finalCodeString
         
-        
+    #verbose gets a variety of information in case the user requires it
     if(verbose):
         verboseInfo.append("hmac(key, counter) in hex: "+HS.hex())
         verboseInfo.append("Offset is: "+str(offset))
@@ -450,7 +532,7 @@ def hotp_algorithm(key, count):
     return finalCode
     
     
-
+#determine whether the user wants TOTP or HOTP
 def main_calculation():
     global totp
     global sharedKey
@@ -466,6 +548,7 @@ def main_calculation():
 
     return result
 
+#The verbose function collects general information after all the arguments have been passed
 def collect_general_information():
     global totp
     global keyEncoding
@@ -499,7 +582,8 @@ def collect_general_information():
     verboseInfo.append("\n---------- Algorithm Calculations Below --------------------\n")
     
     return
-    
+
+#a small function to print all the verbose information
 def print_verbose_information():
     global verboseInfo
     
@@ -507,21 +591,26 @@ def print_verbose_information():
         print(verboseInfo[x])
     print("\n---------- Verbose Information Finished --------------------\n")
 
+#main function
 def main():
     global verbose
     proceed = handleArgs()
     result  = 0
 
+    #if the arguments have been properly handled. Proceed in the execution of the program
+    #else exit the program
     if(proceed == 1):
         proper_usage()
         sys.exit()
     else:
+        #collect verbose information if required to
         if(verbose):
             collect_general_information()
         result = main_calculation()
 
         if(verbose):
             print_verbose_information()
+        #finally print the code
         print("This is the code: "+result)
 
 if __name__ == "__main__":
